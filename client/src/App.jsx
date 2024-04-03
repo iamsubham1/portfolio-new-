2eb6083a82d528b32c60b16173f807c65cc529d4
subham das <das1998lipun@gmail.com>
2024-04-03 22:19:12 +0530

    ui changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import Container from './components/Container';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
--- client/src/components/PhoneComponent.jsx
+++ client/src/components/PhoneComponent.jsx
@@ -12,9 +12,9 @@ const PhoneComponent = () => {
 
     return (
         <>
-            <div className='phoneContainer flex gap-[35vw]'>
-                <img src={iphone} className='relative' style={style} />
-                <img src={myPic} className='relative max-h-[65%] top-[8%]' />
+            <div className='phoneContainer mobile flex gap-[35vw] bg-[#9DC32F]'>
+                <img src={iphone} className='relative max-w-[100%] phone top-[12%] left-[12%] h-[84%]' />
+                <img src={myPic} className='relative max-h-[65%] top-[8%] myimg' />
             </div>
             <div className='banner bg-[#181717] z-99 relative top-[-30%] max-h-20 text-[#4e4e4e]' >
                 <p class="marquee">
